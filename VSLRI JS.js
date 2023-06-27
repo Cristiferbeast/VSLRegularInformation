d82634e50a2cf8ac376b5f92210fcf8c293dc1fe
Cristiferbeast <121412458+Cristiferbeast@users.noreply.github.com>
2023-06-26 22:34:25 -0400

    Update VSLRI JS.js

--- VSLRI JS.js
+++ VSLRI JS.js
@@ -6,3 +6,20 @@ function reinbursementVigor() {
 	var result = (number * .2) + (num2);
 	document.getElementById("resultOutput").innerHTML = "You would get " + result + " resources back";
   }
+function searchHeadings() {
+    var searchInput = document.getElementById("searchInput").value.toLowerCase();
+    var headings = document.querySelectorAll("h1, h2, h3");
+    var resultsContainer = document.getElementById("results");
+    resultsContainer.innerHTML = "";
+
+    for (var i = 0; i < headings.length; i++) {
+        var title = headings[i].innerText.toLowerCase();
+        if (title.includes(searchInput)) {
+            var link = document.createElement("a");
+            link.href = "#" + headings[i].id;
+            link.textContent = headings[i].innerText;
+            resultsContainer.appendChild(link);
+            resultsContainer.appendChild(document.createElement("br"));
+        }
+    }
+}
